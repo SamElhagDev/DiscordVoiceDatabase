8aa2b4695be2175b73b86e4e7bb441c7732e3f29
Sam Elhag <sami.eltaj.elhag@gmail.com>
2025-08-22 23:14:26 -0400

    Progress Configuring Bot

--- bot.py
+++ bot.py
@@ -28,9 +28,9 @@ Setup bot intents (events restrictions)
 For more information about intents, please go to the following websites:
 https://discordpy.readthedocs.io/en/latest/intents.html
 https://discordpy.readthedocs.io/en/latest/intents.html#privileged-intents
+"""
 
-
-Default Intents:
+intents =discord.Intents.default()
 intents.bans = True
 intents.dm_messages = True
 intents.dm_reactions = True
@@ -50,13 +50,10 @@ intents.typing = True
 intents.voice_states = True
 intents.webhooks = True
 
-Privileged Intents (Needs to be enabled on developer portal of Discord), please use them only if you need them:
 intents.members = True
 intents.message_content = True
 intents.presences = True
-"""
 
-intents = discord.Intents.default()
 
 """
 Uncomment this if you want to use prefix (normal) commands.
